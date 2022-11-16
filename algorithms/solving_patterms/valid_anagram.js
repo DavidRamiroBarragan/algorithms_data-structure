3d0e25c33819adf2b7658f8facc7308d6dd9e7b8
M. David Ramiro Barragan <dramirobarragan@gmail.com>
2022-11-16 09:35:22 +0000

    valid anagram

---
+++ algorithms/solving_patterms/valid_anagram.js
@@ -0,0 +1,30 @@
+// Given two strings, write a function to determine if the second string is
+// an anagram of the first. An anagram is a word, phrase, or name formed by
+// rearranging the letters of another, such as cinema, formed from iceman.
+validAnagram('', ''); // true
+validAnagram('aaz', 'zza'); // false
+validAnagram('anagram', 'nagaram'); // true
+validAnagram("rat", "car"); // false
+validAnagram('awesome', 'awesom'); // false
+validAnagram('amanaplanacanalpanama', 'acanalmanplanpamana'); // false
+validAnagram('qwerty', 'qeywrt'); // true
+validAnagram('texttwisttime', 'timetwisttext'); // true
+//You may assume the string contains only lowercase alphabets.
+function validAnagram(str1, str2) {
+    if (str1.length !== str2.length) {
+        return false;
+    }
+    var acc = {};
+    for (var i = 0; i <= str1.length; i++) {
+        var letter = str1[i];
+        acc[letter] ? acc[letter] += 1 : acc[letter] = 1;
+    }
+    for (var i = 0; i <= str2.length; i++) {
+        var letter = str2[i];
+        if (acc[letter] === undefined) {
+            return false;
+        }
+        acc[letter] -= 1;
+    }
+    return true;
+}
---
+++ algorithms/solving_patterms/valid_anagram.ts
@@ -0,0 +1,37 @@
+// Given two strings, write a function to determine if the second string is
+// an anagram of the first. An anagram is a word, phrase, or name formed by
+// rearranging the letters of another, such as cinema, formed from iceman.
+
+validAnagram('', ''); // true
+validAnagram('aaz', 'zza'); // false
+validAnagram('anagram', 'nagaram'); // true
+validAnagram('rat', 'car'); // false) // false
+validAnagram('awesome', 'awesom'); // false
+validAnagram('amanaplanacanalpanama', 'acanalmanplanpamana'); // false
+validAnagram('qwerty', 'qeywrt'); // true
+validAnagram('texttwisttime', 'timetwisttext'); // true
+
+//You may assume the string contains only lowercase alphabets.
+
+function validAnagram(str1: string, str2: string) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  
+  const acc: Record<string, number> = {};
+  
+  for (let i = 0; i <= str1.length; i++) {
+    const letter = str1[i];
+    acc[letter] ? acc[letter] += 1 : acc[letter] = 1;
+  }
+  
+  for (let i = 0; i <= str2.length; i++) {
+    const letter = str2[i];
+    if (acc[letter] === undefined) {
+      return false;
+    }
+    
+    acc[letter] -= 1;
+  }
+  return true;
+}
